2a1f1779f5b945937a6d6ff1396fbd6df5e98e95
John Gage Faulkner <46038619+johngagefaulkner@users.noreply.github.com>
2021-10-15 03:51:42 -0400

    Update StringEditor.cs

--- Helpers/StringEditor.cs
+++ Helpers/StringEditor.cs
@@ -5,7 +5,6 @@ namespace Snippets.Helpers
 {
     public class StringEditor
     {
-        // Easily remove Quotation Marks (") and Apostrophes (') from a string.
         public static string RemoveQuotes(string inputStr, bool alsoRemoveApostrophes = false)
         {
             string _result = inputStr.Replace(Punctuation.GetString(Punctuation.PunctuationMark.QuotationMark), "");
@@ -16,6 +15,20 @@ namespace Snippets.Helpers
             }
 
             return _result.Trim();
+        }
+
+        public static string AddQuotes(string inputStr, bool useSingleQuotes = false)
+        {
+            if (useSingleQuotes == true)
+            {
+                return "'" + inputStr.Trim() + "'";
+            }
+
+            else
+            {
+                string _str = Punctuation.PunctuationMark.QuotationMark.ToString() + inputStr.Trim() + Punctuation.PunctuationMark.QuotationMark.ToString();
+                return _str.Trim();
+            }
         }
     }
 }
