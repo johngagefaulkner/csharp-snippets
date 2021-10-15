8cad6f37c97abc1bbf2e6aa7fd829755c7c9750c
John Gage Faulkner <46038619+johngagefaulkner@users.noreply.github.com>
2021-10-15 01:04:37 -0400

    Create StringEditor.cs

---
+++ Helpers/StringEditor.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snippets.Helpers
+{
+    public class StringEditor
+    {
+        public static string GetFilePathWithoutQuotes(string inputFilePath)
+        {
+            char quoteChar = '"';
+            string rev1 = inputFilePath.Replace(quoteChar.ToString(), "'");
+            string rev2 = "'" + rev1.Trim() + "'";
+            return rev2.Trim();
+        }
+    }
+}
