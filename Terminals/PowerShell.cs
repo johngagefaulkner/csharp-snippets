f0d0a1e5c10e4259ffc79b8b93eb697f59513e63
John Gage Faulkner <46038619+johngagefaulkner@users.noreply.github.com>
2021-10-15 03:40:25 -0400

    Update PowerShell.cs

--- Terminals/PowerShell.cs
+++ Terminals/PowerShell.cs
@@ -5,9 +5,11 @@ namespace Snippets.Terminals
 {
   public class PowerShell
   {
-      /* Attempting to keep this code as simple as possible, this class exposes only 2 methods. 
+      /* Attempting to keep this code as simple as possible, this class exposes only 4 methods. 
        *  - [1: ExecuteCommand] Synchronously launches a  PowerShell (powershell.exe) instance and executes the command passed-through as an argument. Returns the redirected StandardOutput.
        *  - [2: ExecuteCommandAsync] Asynchronously launches a PowerShell (powershell.exe) instance and executes the command passed-through as an argument. Awaits then returns the redirected StandardOutput.
+       * - Pending
+       * - Pending
        * Default params were purposely kept basic, feel free to expand as needed.
        * - [0: psCommand] The command or script to execute in the PowerShell instance.
        * - [1: psPolicy] The ExecutionPolicy assigned when launching the PowerShell instance. Default: Bypass.
@@ -92,5 +94,71 @@ namespace Snippets.Terminals
               return "Error: " + ex.Message;
           }
       }
+    
+    /// <summary>
+        /// Synchronously launches a PowerShell instance and executes the script located at the provided file path.
+        /// </summary>
+        /// <param name="filePath">Full path to the .ps1 script.</param>
+        /// <returns>[string] Redirects and returns the StandardOutput into a single trimmed string (or, alternatively, the Exception as a string.)</returns>
+        public static string ExecutePS1(string filePath)
+        {
+            try
+            {
+                string cleansedPath = StringEditor.RemoveQuotes(filePath, true);
+                string psCommand = "Invoke-Command -FilePath " + StringEditor.AddQuotes(cleansedPath, true);
+
+                Process _powerShell = new Process();
+                _powerShell.StartInfo.UseShellExecute = false;
+                _powerShell.StartInfo.RedirectStandardOutput = true;
+                _powerShell.StartInfo.CreateNoWindow = true;
+                _powerShell.StartInfo.Verb = "runas";
+                _powerShell.StartInfo.FileName = "powershell.exe";
+                _powerShell.StartInfo.Arguments = $"-NoProfile -ExecutionPolicy Bypass -NoLogo -Command {psCommand}";
+                _powerShell.Start();
+
+                string _output = _powerShell.StandardOutput.ReadToEnd();
+                _powerShell.WaitForExit();
+
+                return _output.ToString().Trim();
+            }
+
+            catch (Exception ex)
+            {
+                return "Error: " + ex.Message;
+            }
+        }
+
+        /// <summary>
+        /// Asynchronously launches a PowerShell instance and executes the script located at the provided file path.
+        /// </summary>
+        /// <param name="filePath">Full path to the .ps1 script.</param>
+        /// <returns>[string] Redirects and returns the StandardOutput into a single trimmed string (or, alternatively, the Exception as a string.)</returns>
+        public static async Task<string> ExecutePS1Async(string filePath)
+        {
+            try
+            {
+                string cleansedPath = StringEditor.RemoveQuotes(filePath, true);
+                string psCommand = "Invoke-Command -FilePath " + StringEditor.AddQuotes(cleansedPath, true);
+
+                Process _powerShell = new Process();
+                _powerShell.StartInfo.UseShellExecute = false;
+                _powerShell.StartInfo.RedirectStandardOutput = true;
+                _powerShell.StartInfo.CreateNoWindow = true;
+                _powerShell.StartInfo.Verb = "runas";
+                _powerShell.StartInfo.FileName = "powershell.exe";
+                _powerShell.StartInfo.Arguments = $"-NoProfile -ExecutionPolicy Bypass -NoLogo -Command {psCommand}";
+                _powerShell.Start();
+
+                string _output = await _powerShell.StandardOutput.ReadToEndAsync();
+                await _powerShell.WaitForExitAsync();
+
+                return _output.ToString().Trim();
+            }
+
+            catch (Exception ex)
+            {
+                return "Error: " + ex.Message;
+            }
+        }
   }
 }
