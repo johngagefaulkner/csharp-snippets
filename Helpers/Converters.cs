6e05cf28462959a9903a8d79bbaf57e360100328
John Gage Faulkner <46038619+johngagefaulkner@users.noreply.github.com>
2021-10-15 09:20:36 -0400

    Update Converters.cs

--- Helpers/Converters.cs
+++ Helpers/Converters.cs
@@ -1,13 +1,33 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Snippets.Helpers
 {
     public class Converters
-    {
-        // ToDo: Add code
+    {        
+        // Reads JSON from the provided file path.
+        // Converts the contents to the provided type.
+        public static T ConvertFromJsonFile<T>(string json)
+        {
+            string tmpJson;
+            using (StreamReader sr = new StreamReader(json))
+            {
+                tmpJson = sr.ReadToEnd();
+            }
+
+            return System.Text.Json.Deserialize<T>(tmpJson);
+        }
+
+        // Converts the provided string to the provided type.
+        public static T ConvertFromJsonString<T>(string json)
+        {
+            return System.Text.Json.JsonSerializer.Deserialize<T>(json);
+        }
     }
 }
