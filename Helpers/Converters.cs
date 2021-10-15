da11cf24585b620f07cb5dbe9bcfbb21bb26fc4e
John Gage Faulkner <46038619+johngagefaulkner@users.noreply.github.com>
2021-10-15 01:03:30 -0400

    Update Converters.cs

--- Helpers/Converters.cs
+++ Helpers/Converters.cs
@@ -3,3 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+namespace Snippets.Helpers
+{
+    public class Converters
+    {
+        public static string GetFilePathWithoutQuotes(string inputFilePath)
+        {
+            char quoteChar = '"';
+            string rev1 = inputFilePath.Replace(quoteChar.ToString(), "'");
+            string rev2 = "'" + rev1.Trim() + "'";
+            return rev2.Trim();
+        }
+    }
+}
