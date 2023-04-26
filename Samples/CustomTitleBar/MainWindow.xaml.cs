94edaca43a137489ff7b190499513643a2d25d33
John Gage Faulkner <46038619+johngagefaulkner@users.noreply.github.com>
2023-04-26 01:45:42 -0400

    Update and rename MainWindow.xaml.cs to Samples/CustomTitleBar/MainWindow.xaml.cs

---
+++ Samples/CustomTitleBar/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Globalization;
+using System.Threading.Tasks;
+
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Controls;
+
+namespace SampleCustomTitleBarApp;
+
+public sealed partial class MainWindow : Window
+{
+
+}
