338829739a95b93147c681ba66a9f6aab4e9deda
John Gage Faulkner <46038619+johngagefaulkner@users.noreply.github.com>
2023-05-25 20:00:02 -0400

    Update Networking.cs

--- Networking/Networking.cs
+++ Networking/Networking.cs
@@ -1,35 +1,23 @@
-using System;
 using System.Net.NetworkInformation;
- 
-//namespace Snippets.Networking
+
 namespace Snippets
 {
-    //internal class CheckService
     public partial class Networking
     {
-        // A Tuple allows us to have more values in output
-        public (bool, string) IsReachable(string ipOrAddress)
-        {
-            // The using statement ensures the correct use of IDisposable objects.
-            using (var ping = new Ping())
-            {
-                try
-                {
-                    bool result = true;
-                    // In the method Send we can pass the IP or the Address
-                    var checkPing = ping.Send(ipOrAddress);
-                    if (checkPing.Status != IPStatus.Success)
-                    {
-                        result = false;
-                    }
- 
-                    return (result, checkPing.Status.ToString());
-                }
-                catch(Exception ex)
-                {
-                    return (false, ex.Message) ;
-                }
-            }
-        }
+		public static async Task<(bool, string)> IsIPAddressReachable(string ipAddress) // Tuple allows more values in output, Async allows not locking UI thread
+		{
+			try
+			{
+				using (var ping = new Ping()) // "using" statement ensures correct use of IDisposable objects
+				{
+					var checkPing = await ping.SendPingAsync(ipAddress);
+					return checkPing.Status == IPStatus.Success ? (true, checkPing.Status.ToString()) : (false, checkPing.Status.ToString());
+				}
+			}
+			catch(Exception ex)
+			{
+				return (false, ex.Message) ;
+			}
+		}
     }
 }
