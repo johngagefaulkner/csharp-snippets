56b74914c82b7a9fbd4d73c42bd0e7e646bef464
John Gage Faulkner <46038619+johngagefaulkner@users.noreply.github.com>
2023-06-25 12:47:21 -0400

    Create MainPage.cs

---
+++ Windowing/NavigationViewWithInfoBarSample/MainPage.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Text;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace Snippets
+{
+    public const string SampleCodeReference = "https://github.com/microsoft/WindowsAppSDK-Samples/blob/main/Samples/CustomControls/CsApp/MainPage.xaml";
+    public class NavigationViewWithInfoBarSample()
+    {
+        InitializeComponent();
+    }
+}
