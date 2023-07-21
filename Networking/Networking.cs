cd7e576f88f1b72aa020e48db0522d623963a8d2
John Gage Faulkner <46038619+johngagefaulkner@users.noreply.github.com>
2023-07-20 21:15:04 -0400

    Update Networking.cs

--- Networking/Networking.cs
+++ Networking/Networking.cs
@@ -2,25 +2,35 @@ using System.Net.NetworkInformation;
 
 namespace Snippets
 {
-    public partial class Networking
+    public static class Networking
     {
-		// Tuple allows more values in output, Async allows not locking UI thread
-        public static async Task<(bool, string)> IsIPAddressReachable(string ipAddress)
+        // A Tuple allows us to have more values in output
+        public static (bool, string) SendPing(string ipAddress)
+        {
+            // The using statement ensures the correct use of IDisposable objects.
+            using (var ping = new Ping())
+            {
+                try
+                {
+                    var checkPing = ping.Send(ipAddress);
+                    return (IPStatus.Success, checkPing.Status.ToString());
+                }
+                catch (Exception ex) { return (false, ex.Message); }
+            }
+        }
+
+        // Tuple allows more values in output, Async allows not locking UI thread
+        public static async Task<(bool, string)> SendPingAsync(string ipAddress)
         {
             using (var ping = new Ping()) // The "using" statement ensures correct use of IDisposable objects
             {
                 try
                 {
-                    var checkPing = await ping.SendPingAsync(ipAddress);
-
-                    if (checkPing.Status == IPStatus.Success) return (true, checkPing.Status.ToString());
-                    else return (false, checkPing.Status.ToString());
+			var checkPing = await ping.SendPingAsync(ipAddress);
+ 			return (IPStatus.Success, checkPing.Status.ToString());
                 }
 
-                catch (Exception ex)
-                {
-                    return (false, ex.Message);
-                }
+                catch (Exception ex) { return (false, ex.Message); }
             }
         }
     }
